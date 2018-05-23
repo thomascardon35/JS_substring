4836e88140e76e327bd63c000bda09fface06ece
thomascardon35 <37211499+thomascardon35@users.noreply.github.com>
2018-05-23 23:37:08 +0200

    Create substring.js

---
+++ substring.js
@@ -0,0 +1,15 @@
+<script>
+    $(function(){
+        var tableBox= $("#admin td"); // en natif : document.querySelector("#admin td");
+                                      // Dans mon projet ForEver ce sélecteur est un tableau d'objet d'une quarantaine d'élément
+
+        for (var index = 0; index < tableBox.length; index++){
+
+            var textTableBox = tableBox[index].textContent;
+
+            /*var newTextTableBox = textTableBox.substring(0,3);  */
+            
+            tableBox[index].textContent= textTableBox.substring(0,10) + '...';
+        }
+    });
+</script>
